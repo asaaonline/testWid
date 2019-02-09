ba399ba65d43a8893ce9c6862f8e8b9e8c23fb54
asaonline <asaonline1@gmail.com>
2019-02-09 13:01:41 +0530

    make

---
+++ src/Controllers/ContentController.php
@@ -0,0 +1,15 @@
+<?php
+ 
+namespace testWid\Controllers;
+ 
+ 
+use Plenty\Plugin\Controller;
+use Plenty\Plugin\Templates\Twig;
+ 
+class ContentController extends Controller
+{
+    public function sayHello(Twig $twig):string
+    {
+        return $twig->render('testWid::content.hello');
+    }
+}
